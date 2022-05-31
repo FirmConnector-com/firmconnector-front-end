2cef4c50914463c98b44187080fcc4b748b146ce
Debasish Paul <debasishpaul2014@gmail.com>
2022-05-31 14:10:25 +0530

    backend api url changed

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,8 @@
-const API_BASE = "http://44.237.193.208/index.php/";
+const API_BASE = "https://api.firmconnector.com/index.php/";
 
-const ASSETS_BASE = "http://44.237.193.208/assets/";
-const PROFILE_IMAGE_BASE = "http://44.237.193.208/";
+const ASSETS_BASE = "https://api.firmconnector.com/assets/";
+const PROFILE_IMAGE_BASE = "https://api.firmconnector.com/";
 
-const FIRM_IMAGE_BASE = "http://44.237.193.208/";
+const FIRM_IMAGE_BASE = "https://api.firmconnector.com/";
 
 export { API_BASE, ASSETS_BASE, PROFILE_IMAGE_BASE, FIRM_IMAGE_BASE };
