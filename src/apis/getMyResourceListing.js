26d532dc5896a971b1b9358b749597f1ba6a2e64
Debasish Paul <debasishpaul2014@gmail.com>
2022-06-13 16:57:40 +0530

    resource search

--- src/apis/getMyResourceListing.js
+++ src/apis/getMyResourceListing.js
@@ -3,10 +3,11 @@ import axios from "axios";
 //IMPORT API ROUTE URL
 import { GET_MY_RESOURCE_LISTING } from "./apiRoutes";
 
-const getMyResourceListing = (userSlug) => {
+const getMyResourceListing = (userSlug, filterText) => {
   var postData = new FormData();
 
   postData.append("user_slug", userSlug);
+  postData.append("filter_text", filterText);
 
   return axios
     .post(GET_MY_RESOURCE_LISTING, postData, {
--- src/components/Resources/ResourceListing/ResourceListingBlock.js
+++ src/components/Resources/ResourceListing/ResourceListingBlock.js
@@ -1,69 +1,61 @@
 import BlockHeader from "../../Headers/BlockHeader";
 import ResourceListingResultBlock from "./ResourceListingResultBlock";
-import {Button} from "react-bootstrap";
-import {Link} from "react-router-dom";
-import {useAuthContext} from "../../../context/AuthContext";
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../../../context/AuthContext";
 import "./ResourceListingBlockCss.css";
 
 const ResourceListingBlock = () => {
-    const {userDetails} = useAuthContext();
-    const firmType = JSON.parse(userDetails).firm_details?.firm_type;
-    const user_primary_role = JSON.parse(userDetails).user_primary_role;
+  const { userDetails } = useAuthContext();
+  const firmType = JSON.parse(userDetails).firm_details?.firm_type;
+  const user_primary_role = JSON.parse(userDetails).user_primary_role;
 
-    const firm_details = JSON.parse(userDetails).firm_details;
+  const firm_details = JSON.parse(userDetails).firm_details;
 
-    const checkResourceTextDisplay = () => {
-        if (firm_details) {
-            return (
-                <Link to="/resources">
-                    <Button variant="warning" size="sm">
-                        {firmType === "1" ? "My Team" : "My Candidates"}
-                    </Button>
-                </Link>
-            );
-        }
-    };
+  const checkResourceTextDisplay = () => {
+    if (firm_details) {
+      return (
+        <Link to="/resources">
+          <Button variant="warning" size="sm">
+            {firmType === "1" ? "My Team" : "My Candidates"}
+          </Button>
+        </Link>
+      );
+    }
+  };
 
-    const displayAddResourceText = () => {
-        if (user_primary_role === "2") {
-            if (firm_details) {
-                return (
-                    <div className="col-12">
-                        <div className="d-flex justify-content-end">
-                            <Col className="d-flex search-setting" xs={7}>
-                                <Form.Control
-                                    id="search-box"
-                                    placeholder="Search for Name,Location or Role"
-                                />
-                            </Col>
-                            <Link to="/add-resource">
-                                <Button variant="primary" size="sm">
-                                    {firmType === "1" ? "Add Candidate" : " Add Team Member"}
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
-                );
-            }
-        }
-    };
-
-    return (
-        <>
-            <div className="d-block mb-3">
-                <BlockHeader title={firmType === "1" ? "My Team" : "My Candidates"}/>
-                <div className="d-flex row">
-                    <div className="col-3">{checkResourceTextDisplay()}</div>
-                    <div className="d-flex justify-content-end col-9">
-                        {displayAddResourceText()}
-                    </div>
-                </div>
+  const displayAddResourceText = () => {
+    if (user_primary_role === "2") {
+      if (firm_details) {
+        return (
+          <div className="col-12">
+            <div className="d-flex justify-content-end">
+              <Link to="/add-resource">
+                <Button variant="primary" size="sm">
+                  {firmType === "1" ? "Add Candidate" : " Add Team Member"}
+                </Button>
+              </Link>
             </div>
-            <ResourceListingResultBlock/>
-        </>
-    );
+          </div>
+        );
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="d-block mb-3">
+        <BlockHeader title={firmType === "1" ? "My Team" : "My Candidates"} />
+        <div className="d-flex row">
+          <div className="col-3">{checkResourceTextDisplay()}</div>
+          <div className="d-flex justify-content-end col-9">
+            {displayAddResourceText()}
+          </div>
+        </div>
+      </div>
+      <ResourceListingResultBlock />
+    </>
+  );
 };
 
 export default ResourceListingBlock;
